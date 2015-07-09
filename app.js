acc2b50cefc6292344fb96c5b97e7feb395c7a23
soyto <alf.ixos@gmail.com>
2015-07-09 10:59:09 +0200

    Added test data

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     $scope.title = 'Title setted up with angular';
 
     $http({
-      url: 'data/06-08-2015.json',
+      url: 'data/06-08-2015/Alquima.json',
       method: 'GET'
     }).success(function(data){
       $scope.alquimaData = data;
