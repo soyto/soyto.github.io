9253b32ca3ad3fa2829e2dd26fbc8b12be539fc4
soyto <alf.ixos@gmail.com>
2015-07-09 10:55:04 +0200

    Added test data

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 (function(ng){
   'use strict';
 
-  ng.module('mainApp', []).controller('main.controller', function($scope){
+  ng.module('mainApp', []).controller('main.controller', function($scope, $http){
     $scope.title = 'Title setted up with angular';
+
+    $http({
+      url: 'data/06-08-2015.json',
+      method: 'GET'
+    }).success(function(data){
+      $scope.alquimaData = data;
+    });
+
   });
 
 })(angular);
