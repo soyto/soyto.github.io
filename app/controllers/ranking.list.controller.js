ec64dbe197c9539d9b2fbedcb8151b2f4c98a1d8
soyto <alf.ixos@gmail.com>
2015-07-10 09:55:58 +0200

    Closes #5

--- app/controllers/ranking.list.controller.js
+++ app/controllers/ranking.list.controller.js
@@ -9,17 +9,39 @@
 
   function index_controller($scope, $location, storedDataService, serverData) {
     $scope._name = CONTROLLER_NAME;
-    $scope.serverData = serverData;
-    $scope.storedDates = storedDataService.storedDates;
-    $scope.searchDate = serverData.date;
+
+    _init();
 
 
-    //On date changed
-    $scope.$watch('searchDate', function(newValue){
-      if(newValue != serverData.date) {
-        $location.path('/ranking/' + serverData.serverName + '/' + newValue);
-      }
-    });
+
+
+
+    function _init() {
+      $scope.serverData = serverData;
+      $scope.storedDates = storedDataService.storedDates;
+      $scope.searchDate = serverData.date;
+
+      _fillVersusTable(serverData.data);
+
+      //On date changed
+      $scope.$watch('searchDate', function(newValue){
+        if(newValue != serverData.date) {
+          $location.path('/ranking/' + serverData.serverName + '/' + newValue);
+        }
+      });
+    }
+
+    function _fillVersusTable(data) {
+      $scope.versusData = [];
+
+      data.elyos.forEach(function(elyosCharacter, idx){
+        var asmodianCharacter = data.asmodians[idx];
+        $scope.versusData.push({
+          elyo: elyosCharacter,
+          asmodian: asmodianCharacter
+        });
+      });
+    }
   }
 
 })(angular);
