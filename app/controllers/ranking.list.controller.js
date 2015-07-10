aff4a7fe1544a2000a26230659344e631608e884
soyto <alf.ixos@gmail.com>
2015-07-10 15:27:37 +0200

    Added search Fn

--- app/controllers/ranking.list.controller.js
+++ app/controllers/ranking.list.controller.js
@@ -4,11 +4,13 @@
   var CONTROLLER_NAME = 'mainApp.ranking.list.controller';
 
   ng.module('mainApp').controller(CONTROLLER_NAME, [
-    '$scope', '$location', 'storedDataService', 'serverData', index_controller
+    '$scope', '$location', '$timeout',  'storedDataService', 'serverData', index_controller
   ]);
 
-  function index_controller($scope, $location, storedDataService, serverData) {
+  function index_controller($scope, $location, $timeout, storedDataService, serverData) {
     $scope._name = CONTROLLER_NAME;
+    var initialVersusData = [];
+    var textSearch_timeoutPromise = null;
 
     _init();
 
@@ -20,15 +22,21 @@
       $scope.searchDate = serverData.date;
       $scope.currentServer = storedDataService.serversList.first(function(server){ return server.name == serverData.serverName; });
 
-      $scope.elyosData = serverData.data.elyos;
-      $scope.asmodianData = serverData.data.asmodians;
+      $scope.elyosData = serverData.data.elyos.select(_initCharacter);
+      $scope.asmodianData = serverData.data.asmodians.select(_initCharacter);
 
+      $scope.textSearch = '';
 
-      $scope.elyosData.select(_initCharacter);
-      $scope.asmodianData.select(_initCharacter);
+      serverData.data.elyos.forEach(function(elyosCharacter, idx){
+        var asmodianCharacter = serverData.data.asmodians[idx];
 
+        initialVersusData.push({
+          elyo: _initCharacter(elyosCharacter),
+          asmodian: _initCharacter(asmodianCharacter)
+        });
+      });
 
-      _fillVersusTable(serverData.data);
+      $scope.versusData = initialVersusData;
 
       //On date changed
       $scope.$watch('searchDate', function(newValue){
@@ -42,19 +50,8 @@
           $location.path('/ranking/' + newValue.name + '/' + serverData.date);
         }
       });
-    }
 
-    function _fillVersusTable(data) {
-      $scope.versusData = [];
-
-      data.elyos.forEach(function(elyosCharacter, idx){
-        var asmodianCharacter = data.asmodians[idx];
-
-        $scope.versusData.push({
-          elyo: _initCharacter(elyosCharacter),
-          asmodian: _initCharacter(asmodianCharacter)
-        });
-      });
+      $scope.$watch('textSearch', _performSearch);
     }
 
     function _initCharacter(character){
@@ -65,6 +62,36 @@
       character.soldierRank = storedDataService.getCharacterRank(character.soldierRankID);
 
       return character;
+    }
+
+    function _performSearch(text) {
+
+      if(textSearch_timeoutPromise) {
+        $timeout.cancel(textSearch_timeoutPromise);
+      }
+
+      textSearch_timeoutPromise = $timeout(function(){
+        if(text) {
+
+          $scope.elyosData = serverData.data.elyos.where(function (character) {
+            return character.characterName.toLowerCase().indexOf(text.toLowerCase()) >= 0;
+          }).select(_initCharacter);
+
+          $scope.asmodianData = serverData.data.asmodians.where(function (character) {
+            return character.characterName.toLowerCase().indexOf(text.toLowerCase()) >= 0;
+          }).select(_initCharacter);
+
+          $scope.versusData = initialVersusData.where(function(pair){
+            return pair.elyo.characterName.toLowerCase().indexOf(text.toLowerCase()) >= 0 || pair.asmodian.characterName.toLowerCase().indexOf(text.toLowerCase()) >= 0;
+          });
+
+
+        } else {
+          $scope.elyosData = serverData.data.elyos.select(_initCharacter);
+          $scope.asmodianData = serverData.data.asmodians.select(_initCharacter);
+          $scope.versusData = initialVersusData;
+        }
+      }, 500);
     }
   }
 
