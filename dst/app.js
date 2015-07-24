6dc75c77611207fd34313870c10fd0fd795282bc
soyto <alf.ixos@gmail.com>
2015-07-24 13:32:27 +0200

    (07-25-2015 rankging data) (0.3.11)

--- dst/app.js
+++ dst/app.js
@@ -1,5 +1,5 @@
 /*
- * Soyto.github.io (0.3.9)
+ * Soyto.github.io (0.3.10)
  * 				DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  * 					Version 2, December 2004
  * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
@@ -27,7 +27,8 @@ window.storedDates = [
  '07-20-2015',
  '07-21-2015',
  '07-22-2015',
- '07-23-2015'
+ '07-23-2015',
+ '07-24-2015'
 ];
 
 (function(ng, navigator){
