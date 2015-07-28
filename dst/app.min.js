dc91436b6bb9ab1d94345343579771856332ad2a
soyto <alf.ixos@gmail.com>
2015-07-28 15:03:29 +0200

    (28-07-2015 characterInfo data) (0.4.2)

--- dst/app.min.js
+++ dst/app.min.js
@@ -1,5 +1,5 @@
 /*
- * Soyto.github.io (0.4.0)
+ * Soyto.github.io (0.4.1)
  * 				DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  * 					Version 2, December 2004
  * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
