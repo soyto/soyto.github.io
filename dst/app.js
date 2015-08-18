8e653eb81e24269072a62cd3d9f67bb042153a8e
soyto <alf.ixos@gmail.com>
2015-08-18 15:47:11 +0200

    (08-18-1015 data) (0.4.29)

--- dst/app.js
+++ dst/app.js
@@ -1,5 +1,5 @@
 /*
- * Soyto.github.io (0.4.27)
+ * Soyto.github.io (0.4.28)
  * 				DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  * 					Version 2, December 2004
  * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
@@ -52,7 +52,8 @@ window.storedDates = [
  '08-14-2015',
  '08-15-2015',
  '08-16-2015',
- '08-17-2015'
+ '08-17-2015',
+ '08-18-2015'
 ];
 
 (function(ng, navigator){
