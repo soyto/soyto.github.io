2a587e8d3859294deb005ff59e422263f1c2b469
soyto <alf.ixos@gmail.com>
2015-09-12 21:20:48 +0200

    Canges for version: 0.7.6

--- dst/app.js
+++ dst/app.js
@@ -1798,6 +1798,30 @@
 
 
 (function(ng){
+	'use strict';
+
+	ng.module('mainApp').service('consoleService',[
+		'$window', '$q', 'helperService', 'storedDataService', consoleService
+	]);
+
+
+	function consoleService($window, $q, helperService, storedDataService) {
+
+		var $this = this;
+		$window.soyto = $this;
+
+
+		//Expose $q
+		$this.$q = $q;
+
+		//Expose whole service
+		$this.storedDataService = storedDataService;
+
+
+	}
+})(angular);
+
+(function(ng){
   'use strict';
 
   ng.module('mainApp').service('helperService', [
