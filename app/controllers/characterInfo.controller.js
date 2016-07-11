4296490677a8b82101e45037d2ae60e007475141
soyto <alf.ixos@gmail.com>
2016-07-11 12:25:50 +0200

    (Nacka request) (0.9.3)

--- app/controllers/characterInfo.controller.js
+++ app/controllers/characterInfo.controller.js
@@ -37,14 +37,20 @@
 		  $scope.character.status = $scope.character.status.sort(_dateSortFn);
 		  $scope.character.guilds = $scope.character.guilds.sort(_dateSortFn);
 
-      		  //TODO Requested by Daxking, he doesnt' want to they old name appear
+			//TODO Requested by Daxking, he doesnt' want to they old name be shown
 		  if(characterInfo.serverName == 'Hellion' && characterInfo.characterID == 430586) {
 		    $scope.character.names.splice(1, 1);
 		  }
-		  //TODO Requested by Nyle, he doesnt' want to they old name appear
+
+		  //TODO Requested by Nyle, he doesnt' want to they old name be shown
 		  if(characterInfo.serverName == 'Deyla' && characterInfo.characterID == 825556) {
 		    $scope.character.names.splice(1, 1);
 		  }
+
+      //TODO Requested by Nacka, he doesn't want to they old name be shown
+      if(characterInfo.serverName == 'Urtem' && characterInfo.characterID == 1508483) {
+        $scope.character.names.splice(1, 1);
+      }
 
 		  $scope.character.status.forEach(function(status){
 			  status.soldierRank = storedDataService.getCharacterRank(status.soldierRankID);
