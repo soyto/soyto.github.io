436844411deb32f9ff3836105d5d39762c5bd7b6
soyto <alf.ixos@gmail.com>
2016-12-23 12:16:03 +0100

    (Sumie) (0.10.18)

--- app/services/characterPics.service.js
+++ app/services/characterPics.service.js
@@ -21,6 +21,7 @@
       {'server': 'Antriksha', 'id': 503001, 'pic': '//i.imgur.com/4XBIv3P.png'}, //Antriksha: Livo
       {'server': 'Antriksha', 'id': 600257, 'pic': '//i.imgur.com/qWxds5G.gif'}, //Antriksha: Lember
       {'server': 'Antriksha', 'id': 457727, 'pic': '//i.imgur.com/SHVY2ef.png'}, //Antriksha: Riborn
+      {'server': 'Deyla', 'id': 1236631, 'pic': '//i.imgur.com/CUBmmYW.jpg'}, //Deyla: Sumie
     ];
 
     //Sets a character pic
