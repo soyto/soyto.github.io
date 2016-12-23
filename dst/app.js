e4f598326d4f52bfa324da3d3f4fe4de0204760c
soyto <alf.ixos@gmail.com>
2016-12-23 15:07:09 +0100

    (Ashuramaru) (0.10.30)

--- dst/app.js
+++ dst/app.js
@@ -1881,6 +1881,7 @@
       {'server': 'Hellion', 'id': 288297, 'pic': '//i.imgur.com/6xyFDTJ.png'}, //Hellion: Yleath
       {'server': 'Hellion', 'id': 547988, 'pic': '//i.giphy.com/9wZMlnM0R06l2.gif'}, //Hellion: Tendeeeeeee
       {'server': 'Hellion', 'id': 430842, 'pic': '//i.giphy.com/NVIowdX8ePh4Y.gif'}, //Hellion: Powatrona
+      {'server': 'Hellion', 'id': 361870, 'pic': '//i.giphy.com/dTVg9EKN6ZfJ6.gif'}, //Hellion: Ashuramaru
       {'server': 'Antriksha', 'id': 503001, 'pic': '//i.imgur.com/4XBIv3P.png'}, //Antriksha: Livo
       {'server': 'Antriksha', 'id': 600257, 'pic': '//i.imgur.com/qWxds5G.gif'}, //Antriksha: Lember
       {'server': 'Antriksha', 'id': 457727, 'pic': '//i.imgur.com/SHVY2ef.png'}, //Antriksha: Riborn
