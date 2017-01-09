a21adcad68f70c473fc3afdbfbfa0397224f5b0b
soyto <alf.ixos@gmail.com>
2017-01-09 11:53:51 +0100

    (Theknight... antriksha) (0.11.7)

--- dst/app.js
+++ dst/app.js
@@ -1,5 +1,5 @@
 /*
- * Soyto.github.io (0.11.5)
+ * Soyto.github.io (0.11.6)
  *     DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  *         Version 2, December 2004
  * 
@@ -1898,6 +1898,7 @@
       {'server': 'Deyla', 'id': 1236631, 'pic': '//i.imgur.com/fSTG5mc.png'}, //Deyla: Sumie
       {'server': 'Deyla', 'id': 1266763, 'pic': '//i.imgur.com/5rU4kmQ.png'}, //Deyla: Kaijur
       {'server': 'Hellion', 'id': 121280, 'pic': '//i.imgur.com/t5bDYnw.png'}, //Hellion: Sureh
+      {'server': 'Antriksha', 'id': 135676, 'pic': '//i.imgur.com/S3h7pXH.png'}, //Antriksha: TheKnight... 
     ];
 
     //Sets a character pic
