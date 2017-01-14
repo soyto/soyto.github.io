76c33907fe7d529a0424c5755bedf2e7f4eee9cf
soyto <alf.ixos@gmail.com>
2017-01-14 11:55:53 +0100

    (Show facebook coment tool on mobile (Testing if not working properly i will delete it :v:)) (0.13.40)

--- app/directives/facebookCommentBox.directive.js
+++ app/directives/facebookCommentBox.directive.js
@@ -3,29 +3,28 @@
 
   ng.module('mainApp').directive('fbCommentPlugin', ['$window', function($window)  {
     function createHTML(href, numposts, colorscheme) {
-        return '<div class="fb-comments" ' +
-                       'data-href="' + href + '" ' +
-                       'data-numposts="' + numposts + '" ' +
-                       'data-colorsheme="' + colorscheme + '">' +
-               '</div>';
+      return '<div class="fb-comments" ' +
+        'data-href="' + href + '" ' +
+        'data-numposts="' + numposts + '" ' +
+        'data-colorsheme="' + colorscheme + '">' +
+        '</div>';
     }
-
     return {
-        restrict: 'A',
-        scope: {},
-        link: function postLink(scope, elem, attrs) {
-            attrs.$observe('pageHref', function (newValue) {
-                var href        = newValue;
-                var numposts    = attrs.numposts    || 5;
-                var colorscheme = attrs.colorscheme || 'light';
+      'restrict': 'A',
+      'scope': {},
+      'link': function postLink(scope, elem, attrs) {
+        attrs.$observe('pageHref', function (newValue) {
+          var href        = newValue;
+          var numposts    = attrs.numposts    || 5;
+          var colorscheme = attrs.colorscheme || 'light';
 
-                elem.html(createHTML(href, numposts, colorscheme));
+          elem.html(createHTML(href, numposts, colorscheme));
 
-                if($window.FB) {
-                  $window.FB.XFBML.parse(elem[0]);
-                }
-            });
-        }
+          if($window.FB) {
+            $window.FB.XFBML.parse(elem[0]);
+          }
+        });
+      }
     };
   }]);
 
